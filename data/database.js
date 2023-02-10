ef2373db22817d14a406744faad2a3b79087a77a
Nirnay Behera <nirnay.1behera@gmail.com>
2023-02-10 21:05:16 +0530

    first commit

---
+++ data/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    database: 'blog',
+    user: 'root',
+    password: 'Eva_Unit01'
+});
+
+module.exports = pool;
