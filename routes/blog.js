df461a03874a2606a2658165b2c8c7b324718848
Nirnay Behera <nirnay.1behera@gmail.com>
2023-02-11 16:39:50 +0530

    added detailed page

--- routes/blog.js
+++ routes/blog.js
@@ -36,4 +36,30 @@ router.post('/posts',async function(req,res){
     res.redirect('/posts');
 });
 
+router.get('/posts/:id',async function(req,res){
+    const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id=?
+    `;
+    const [posts] = await db.query(query, [req.params.id]);
+
+    if(!posts || posts.length === 0){
+        return res.status(404).render('404');
+    }
+
+    const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        humanReadableDate: posts[0].date.toLocaleDateString('en-US',{
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        })
+    }
+
+    res.render('post-detail', {post:postData} );
+});
+
 module.exports = router;
